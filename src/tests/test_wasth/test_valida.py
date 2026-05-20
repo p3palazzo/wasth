cd910079f1115b43eaec3468606d3ede17d6484a
Pedro P. Palazzo <palazzo@unb.br>
2026-05-20 16:17:39 -0300

    Frontmatter linting funcionando

---
+++ src/tests/test_wasth/test_valida.py
@@ -0,0 +1,16 @@
+import pytest
+import wasth.valida
+
+@pytest.fixture
+def testfile():
+    f = "testdata/casa/br_df-planaltina-casarao_azul.md"
+    return f
+
+def test_f_read(testfile):
+    assert type(wasth.valida.f_read(testfile)) is dict
+
+def test_prt_title(testfile):
+    assert wasth.valida.prt_title(testfile) == "Casarão Azul"
+
+def test_f_lint(testfile):
+    assert type(wasth.valida.f_lint(testfile)) is list
--- src/wasth/valida.py
+++ src/wasth/valida.py
@@ -16,54 +16,57 @@ yaml = YAML(typ='safe')
 # import pandas as pd
 # import geopandas as gpd
 
-def f_read(f, mode='r', enc="utf-8") -> None:
+def f_read(f, mode='r', enc="utf-8") -> dict:
+    """Lê o arquivo/ficheiro se ele não estiver vazio"""
     with open(f, 'r', encoding=enc) as f:
-        read_data = f.read()
-        return read_data
+        contents = f.read().split('\n...\n\n', 2)
+        frontmatter = contents[0] + '\n'
+        body = contents[1].lstrip() or ''
+        document = {
+            'frontmatter': frontmatter.lstrip(),
+            'body': body
+        }
+    return document
 
 def prt_title(f) -> str:
-    """
-    TODO: lidar com arquivos markdown que têm conteúdo após o bloco YAML
-    """
-    data = yaml.load(str(f_read(f)))
-    return data['title']
+    return yaml.load(f_read(f)['frontmatter'])['title']
 
-def f_lint(f) -> None:
+def f_lint(f) -> list:
     """Mostra os problemas de formatação"""
-    import yamllint.config
-    import yamllint.linter
-    yaml_config = yamllint.config.YamlLintConfig("extends: relaxed")
-    yaml_lint = yamllint.linter.run(open(f, "r"), yaml_config)
-    if yaml_lint == 0:
-        print("Nenhum problema detetado no arquivo/ficheiro.\n")
-    elif yaml_lint == 1:
-        print("Erros de formatação detetados.\n")
-    elif yaml_lint == 2:
-        print("Nenhum erro detetado, mas inconsistências de formatação.\n")
+    frontmatter = f_read(f)['frontmatter']
     print(
         "--------------------------------------------------------------------",
         "\n",
         prt_title(f),
         "\n"
     )
+    import yamllint.config
+    import yamllint.linter
+    yaml_config = yamllint.config.YamlLintConfig("extends: relaxed")
+    yaml_lint = yamllint.linter.run(frontmatter, yaml_config)
+    yaml_lint_list = []
     for p in yaml_lint:
-        if p.level == "error":
-            p_level = "❌ "
-        elif p.level == "warning":
-            p_level = "⚠️"
-        else:
-            p_level = p.level
+        yaml_lint_list.append(p)
+        match p.level:
+            case "error":
+                p_level = "❌ "
+            case "warning":
+                p_level = "⚠️"
+            case _:
+                p_level = p.level
         print(
             p_level,
-            f"{p.line:>4}{':'}",
+            f"{p.line:>4}{':'}{p.column:>2}",
             f"{p.desc:<40}",
             f"{'['}{p.rule}{']'}"
         )
+    return yaml_lint_list
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        for arg in sys.argv[1:]:
-            if os.path.isfile(arg):
-                f_lint(arg)
+        args = sys.argv[1]
     else:
-        print("Informar nome(s) de arquivo/ficheiro")
+        args = input("Informar nome(s) de arquivo/ficheiro:\n").split()
+    for arg in args:
+        if os.path.isfile(arg):
+            f_lint(arg)
